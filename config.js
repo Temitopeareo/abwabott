fd53ce0d480e5739349c83dc5e3fea6a87686a2b
Temitopeareo <132304443+Temitopeareo@users.noreply.github.com>
2024-05-12 04:09:38 -0700

    ABRO DEV

--- config.js
+++ config.js
@@ -6,20 +6,20 @@ global.audio = "";
 global.video = "";
 global.port = process.env.PORT;
 global.appUrl = process.env.APP_URL || "";
-global.email = "ngoyaibaaj@gmail.com";
-global.location = "Bungoma,Kenya.";
+global.email = "abrahamtemitope247@gmail.com";
+global.location = "Lagos,Nigeria.";
 global.mongodb = process.env.MONGODB_URI || "";
 global.allowJids = process.env.ALLOW_JID || "null";
 global.blockJids = process.env.BLOCK_JID || "null";
 global.DATABASE_URL = process.env.DATABASE_URL || "";
-global.timezone = process.env.TZ || process.env.TIME_ZONE || "Africa/Nairobi";
-global.github = process.env.GITHUB || "https://github.com/kingibrahimtech/Black-panther-md-x";
-global.gurl = process.env.GURL || "https://whatsapp.com/channel/0029VaZuGSxEawdxZK9CzM0Y";
-global.website = process.env.GURL || "https://whatsapp.com/channel/0029VaZuGSxEawdxZK9CzM0Y";
-global.THUMB_IMAGE = process.env.THUMB_IMAGE || process.env.IMAGE || "https://telegra.ph/file/7260c0965d835f26fad48.jpg";
-global.devs = "254710772666";
-global.sudo = process.env.SUDO || "254710772666";
-global.owner = process.env.OWNER_NUMBER || "254710772666";
+global.timezone = process.env.TZ || process.env.TIME_ZONE || "Africa/Lagos";
+global.github = process.env.GITHUB || "https://github.com/Temitopeareo/abwabott";
+global.gurl = process.env.GURL || "";
+global.website = process.env.GURL || "";
+global.THUMB_IMAGE = process.env.THUMB_IMAGE || process.env.IMAGE || "https://imgur.com/sm0w91L.jpeg";
+global.devs = "2348100151048";
+global.sudo = process.env.SUDO || "2348100151048";
+global.owner = process.env.OWNER_NUMBER || "2348100151048";
 global.style = process.env.STYLE || "1";
 global.gdbye = process.env.GOODBYE || "false";
 global.wlcm = process.env.WELCOME || "false";
@@ -27,15 +27,15 @@ global.warncount = process.env.WARN_COUNT || 3;
 global.disablepm = process.env.DISABLE_PM || "false";
 global.disablegroup = process.env.DISABLE_GROUPS || "false",
 global.MsgsInLog = process.env.MSGS_IN_LOG || "true";
-global.userImages = process.env.USER_IMAGES || "https://telegra.ph/file/ec8ce4af1d8fa3271e26a.jpg,https://telegra.ph/file/ec059aa5db556bbd951c6.jpg";
+global.userImages = process.env.USER_IMAGES || "https://imgur.com/sm0w91L.jpeg";
 global.waPresence = process.env.WAPRESENCE || "online";
 global.readcmds = process.env.READ_COMMAND || "false";
 global.readmessage = process.env.READ_MESSAGE || "false";
 global.readmessagefrom = process.env.READ_MESSAGE_FROM || "";
 global.read_status = process.env.AUTO_READ_STATUS || "true";
 global.save_status = process.env.AUTO_SAVE_STATUS || "false";
-global.save_status_from = process.env.SAVE_STATUS_FROM || "25739937062,254710772666";
-global.read_status_from = process.env.READ_STATUS_FROM || "25739937062,254710772666";
+global.save_status_from = process.env.SAVE_STATUS_FROM || "25739937062,2348100151048";
+global.read_status_from = process.env.READ_STATUS_FROM || "25739937062,2348100151048";
 
 global.api_smd = "https://api-smd-1.vercel.app";
 global.scan = "https://ibrahim-tech-qr-1-2-1.onrender.com/";
@@ -48,12 +48,12 @@ module.exports = {
   HANDLERS: process.env.PREFIX || ".",
   BRANCH: process.env.BRANCH || "main",
   VERSION: process.env.VERSION || "1.0.0",
-  caption: process.env.CAPTION || "𝐵𝐿𝐴𝐶𝐾 𝑃𝐴𝑁𝑇𝐻𝐸𝑅 𝘟5",
-  author: process.env.PACK_AUTHER || "𝑃𝐴𝑁𝑇𝐻𝐸𝑅",
+  caption: process.env.CAPTION || "ABWABOTT",
+  author: process.env.PACK_AUTHER || "ABRO",
   packname: process.env.PACK_NAME || "🐯",
-  botname: process.env.BOT_NAME || "𝐁𝐋𝐀𝐂𝐊 𝐏𝐀𝐍𝐓𝐇𝐄𝐑 𝐌𝐃",
-  ownername: process.env.OWNER_NAME || "𝑰𝒃𝒓𝒂𝒉𝒊𝒎 𝑨𝒅𝒂𝒎𝒔",
-  errorChat: process.env.ERROR_CHAT || "254710772666",
+  botname: process.env.BOT_NAME || "ABWABOTT 𝐌𝐃",
+  ownername: process.env.OWNER_NAME || "Abro Tech",
+  errorChat: process.env.ERROR_CHAT || "23488100151048",
   KOYEB_API: process.env.KOYEB_API || "false",
   REMOVE_BG_KEY: process.env.REMOVE_BG_KEY || "",
   OPENAI_API_KEY: process.env.OPENAI_API_KEY || "",
